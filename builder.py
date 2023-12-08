6aa607676ca29cc5699b8e73a2cb0455aa101d06
hennanedouaa <ld_hennane@esi.dz>
2023-12-08 13:50:56 +0100

    non det

--- builder.py
+++ builder.py
@@ -12,13 +12,13 @@ class StateMachine:
         if state in self.states:
             self.start_state = state
         else:
-            print("etat inexistante .")
+            print("Etat inexistante.")
 
     def add_final_state(self, state):
         if state in self.states:
             self.final_states.add(state)
         else:
-            print("etet inexistante.")
+            print("Etat inexistante.")
 
     def add_transition(self, from_state, to_state, label):
         if from_state in self.states and to_state in self.states:
@@ -26,14 +26,14 @@ class StateMachine:
                 self.transitions[from_state] = []
             self.transitions[from_state].append((to_state, label))
         else:
-            print(" l'un des etates est  inexistante")
+            print("L'un des etats est inexistante")
 
     def visualize(self):
         if not self.start_state:
-            print("l'etat initial inexistante ")
+            print("L'etat initial inexistante")
             return
 
-        print("\nl'automate:")
+        print("\nL'automate:")
         paths = []
 
         def traverse(current_state, path):
@@ -56,60 +56,66 @@ class StateMachine:
         for path in paths:
             print(f"Path: {path}")
 
-    def test_string(self, test_str):
-        current_state = self.start_state
-        for char in test_str:
-            transition_found = False
+    def test_string_nd(self, test_str):
+        stack = [(self.start_state, 0, [])]
+
+        while stack:
+            current_state, pos, path = stack.pop()
+
+            if pos == len(test_str):
+                if current_state in self.final_states:
+                    return True, "Accepté", path
+                continue
+
             if current_state in self.transitions:
+                char = test_str[pos]
                 for to_state, label in self.transitions[current_state]:
                     if label == char:
-                        current_state = to_state
-                        transition_found = True
-                        break
-            if not transition_found:
-                return False, "Bloqué : Aucune transition pour le caractère actuel."
+                        stack.append((to_state, pos + 1, path + [f"{current_state} --({label})--> {to_state}"]))
 
-        return current_state in self.final_states, "Accepté" if current_state in self.final_states else "Rejeté : La dernière transition ne mène pas à l'état final."
+        return False, "Rejeté : Aucun chemin valide trouvé", path
 
 def main():
     fsm = StateMachine()
 
     while True:
-        print("\n menu:")
-        print("1. ajouter etat")
-        print("2.  Définir l'état initial ")
-        print("3. Définir l'état final ")
-        print("4. Ajouter  transition")
+        print("\nMenu:")
+        print("1. Ajouter etat")
+        print("2. Définir l'état initial")
+        print("3. Définir l'état final")
+        print("4. Ajouter transition")
         print("5. Visualizer")
         print("6. Exit")
-        print("7. Tester  un mot ")
+        print("7. Tester un mot")
 
-        choice = input("Entrez votre choix :")
+        choice = input("Entrez votre choix: ")
 
         if choice == "1":
-            state = input("Entre ke nom de l'etat: ")
+            state = input("Entrez le nom de l'etat: ")
             fsm.add_state(state)
         elif choice == "2":
-            start_state = input("Entre l'etet initial: ")
+            start_state = input("Entrez l'etat initial: ")
             fsm.set_start_state(start_state)
         elif choice == "3":
-            final_state = input("Entre l'etet final: ")
+            final_state = input("Entrez l'etat final: ")
             fsm.add_final_state(final_state)
         elif choice == "4":
-            from_state = input(" De: ")
+            from_state = input("De: ")
             to_state = input("Vers: ")
-            label = input("le caracter: ")
+            label = input("Le caractere: ")
             fsm.add_transition(from_state, to_state, label)
         elif choice == "5":
             fsm.visualize()
         elif choice == "7":
-            test_str = input("Entre le mot a tester: ")
-            accepted, message = fsm.test_string(test_str)
-            print(f"Result: {'Accepté' if accepted else 'Rejecté'} - {message}")
+            test_str = input("Entrez le mot a tester: ")
+            accepted, message, path = fsm.test_string_nd(test_str)
+            print(f"Resultat: {'Accepté' if accepted else 'Rejeté'} - {message}")
+            if accepted:
+                print("Chemin:", " -> ".join(path))
         elif choice == "6":
             break
         else:
-            print("choix Invalid . réessayer .")
+            print("Choix invalide. Reessayez.")
 
 if __name__ == "__main__":
     main()
